4351215c0df5ee0158d2c426d409fbf6ad2bc1e1
ViniciusMachado <viniciusmachadofragamatos8@gmail.com>
2025-09-16 22:29:33 -0300

    Aula 2 pdfs 1 e 2

---
+++ Aula2/src/aula2/Aula2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aula2;
+
+/**
+ *
+ * @author vinicius_25870
+ */
+public class Aula2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
